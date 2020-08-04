a0a1ffe3b399fc44d52ffc76c4f29ed010984d75
Aisha Gregg <aishagregg@gmail.com>
2020-08-04 22:59:51 +0200

    work on blog home layout

---
+++ pages/README.md
@@ -0,0 +1,18 @@
+<p>
+                With more than four years experience in the customer service and
+                telecommunications industries, working in a fast-paced
+                environment is a welcomed challenge. With skills in
+                <strong>JavaScript frameworks</strong>, HTML5, CSS3, web
+                applications,
+                <strong>
+                  Git, web components, Figma, AdobeXD, as well as testing
+                  libraries such as Jest{" "}
+                </strong>
+                being a critical asset to any development team is guaranteed.
+                <strong>
+                  Self-determined, goal-oriented and a team-player! Furthermore,
+                  relentlessly ready for new opportunities to learn and to grow.
+                </strong>
+                Check out my blogs to find out more and to dive into the world
+                of tech.
+              </p>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
@@ -10,56 +11,23 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className={styles.blogtitle}>
+          A (somewhat) techie life by A.K Gregg
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className={styles.introduction}>
+          <div>
+            <h3>About</h3>
+            <p className={styles.paragraph}>
+              <strong>Aisha Kamilah Gregg</strong> is a
+              <strong>frontend web developer</strong>, blogger,
+              <strong>UI and visual design lover</strong> and
+              <strong>project management graduate</strong>.
+            </p>{" "}
+          </div>
+          <img src="/vercel.svg" alt="" className={styles.image} />
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
